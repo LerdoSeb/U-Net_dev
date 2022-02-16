87d78b6c8f44c2d13b2523c7094dc515d4e15a44
LerdoSeb <lerdo.sebastian@gmail.com>
2022-02-16 11:36:58 +0100

    Reduced epochs

--- training.py
+++ training.py
@@ -13,7 +13,7 @@ plt.style.use(['science'])
 LEARNING_RATE = 2e-3
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 32
-NUM_EPOCHS = 50
+NUM_EPOCHS = 30
 NUM_WORKERS = 4         # guideline: 4* num_GPU
 IMAGE_HEIGHT = 64       # 1280 originally
 IMAGE_WIDTH = 64        # 1918 originally
