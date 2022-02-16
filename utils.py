d5fd372a83d624ce8f6f5c90592f3e76a4247fd6
LerdoSeb <lerdo.sebastian@gmail.com>
2022-02-16 12:33:36 +0100

    Phase (2) Introduce noisy data with sigma=0.1

--- utils.py
+++ utils.py
@@ -36,7 +36,7 @@ def get_loaders(
     # Consider that the couette solver now requires a desired_timesteps
     # parameter for improved reusabilty
     my_couette_data = my2DCouetteSolver(
-        desired_timesteps=10000, vertical_resolution=couette_dim)
+        desired_timesteps=10000, vertical_resolution=couette_dim, sigma=0.1)
     my_images = my_couette_data[:-1]
     my_masks = my_couette_data[1:]
     my_zip = list(zip(my_images, my_masks))
