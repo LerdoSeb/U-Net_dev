572dd363dc7d642a7b67c8dd77e77fc3829abbfb
LerdoSeb <lerdo.sebastian@gmail.com>
2022-02-20 17:25:15 +0100

    Housekeeping and correction of 'if sigma != 0' to 'if sigma != 0.0'

--- couette_solver.py
+++ couette_solver.py
@@ -12,237 +12,6 @@ def applyNoise(input_array, sigma, u_wall=10):
     return output_array
 
 
-def plotFlowProfile(input_array, wall_height=20):
-
-    if input_array.ndim == 2:
-        t, h = input_array.shape
-
-        v_step = wall_height / (h-1)
-        v_steps = np.arange(0, wall_height + v_step, v_step).tolist()
-
-        plt.plot(input_array[0], v_steps, label='t = 0')
-        plt.plot(input_array[int(0.01*t)],
-                 v_steps, label=f't = {int(0.01*t)}')
-        plt.plot(input_array[int(0.05*t)],
-                 v_steps, label=f't = {int(0.05*t)}')
-        plt.plot(input_array[int(0.1*t)],
-                 v_steps, label=f't = {int(0.1*t)}')
-        plt.plot(input_array[int(0.25*t)],
-                 v_steps, label=f't = {int(0.25*t)}')
-        plt.plot(input_array[int(0.5*t)],
-                 v_steps, label=f't = {int(0.5*t)}')
-        plt.plot(input_array[int(0.75*t)],
-                 v_steps, label=f't = {int(0.75*t)}')
-        plt.plot(input_array[-1], v_steps, label=f't = {t}')
-
-        plt.ylabel('Height $z$')
-        plt.xlabel('Velocity $u$')
-        plt.title('Flow Profile in Y-Direction ($U_{wall}$ in X-Direction)')
-        plt.legend()
-        plt.show()
-
-    if input_array.ndim == 3:
-        t, h, w = input_array.shape
-
-        v_step = wall_height / (w-1)
-        v_steps = np.arange(0, wall_height + v_step, v_step).tolist()
-
-        # , sharex=True, sharey=True)
-        fig, (ax1, ax2) = plt.subplots(2, sharex=True)
-        fig.suptitle(
-            'Flow Profiles in X- and Y-Direction ($U_{wall}$ in X-Direction)', fontsize=10)
-
-        ax1.set_title('Y-Direction', fontsize=8)
-        ax1.set_ylabel('Height $y$')
-
-        ax1.plot(input_array[0][:, int(h/2)], v_steps, label='t = 0')
-        ax1.plot(input_array[int(0.01*t)][:, int(h/2)],
-                 v_steps, label=f't = {int(0.01*t)}')
-        ax1.plot(input_array[int(0.05*t)][:, int(h/2)],
-                 v_steps, label=f't = {int(0.05*t)}')
-        ax1.plot(input_array[int(0.1*t)][:, int(h/2)],
-                 v_steps, label=f't = {int(0.1*t)}')
-        ax1.plot(input_array[int(0.25*t)][:, int(h/2)],
-                 v_steps, label=f't = {int(0.25*t)}')
-        ax1.plot(input_array[int(0.5*t)][:, int(h/2)],
-                 v_steps, label=f't = {int(0.5*t)}')
-        ax1.plot(input_array[int(0.75*t)][:, int(h/2)],
-                 v_steps, label=f't = {int(0.75*t)}')
-        ax1.plot(input_array[-1][:, int(h/2)], v_steps, label=f't = {t}')
-
-        ax2.set_title('X-Direction', fontsize=8)
-        ax2.set_ylabel('Length $x$')
-
-        ax2.plot(input_array[0, int(h/2), :], v_steps, label='t = 0')
-        print('test length values')
-        print(input_array[10, int(h/2), :])
-        ax2.plot(input_array[int(0.01*t), int(h/2), :],
-                 v_steps, label=f't = {int(0.01*t)}')
-        ax2.plot(input_array[int(0.05*t), int(h/2), :],
-                 v_steps, label=f't = {int(0.05*t)}')
-        ax2.plot(input_array[int(0.1*t), int(h/2), :],
-                 v_steps, label=f't = {int(0.1*t)}')
-        ax2.plot(input_array[int(0.25*t), int(h/2), :],
-                 v_steps, label=f't = {int(0.25*t)}')
-        ax2.plot(input_array[int(0.5*t), int(h/2), :],
-                 v_steps, label=f't = {int(0.5*t)}')
-        ax2.plot(input_array[int(0.75*t), int(h/2), :],
-                 v_steps, label=f't = {int(0.75*t)}')
-        ax2.plot(input_array[-1, int(h/2), :], v_steps, label=f't = {t}')
-
-        plt.xlabel('Velocity $u$')
-        plt.legend()
-        plt.show()
-
-    if input_array.ndim == 4:
-        t, d, h, w = input_array.shape
-
-        v_step = wall_height / (w-1)
-        v_steps = np.arange(0, wall_height + v_step, v_step).tolist()
-
-        # , sharex=True, sharey=True)
-        fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
-        fig.suptitle(
-            'Flow Profiles in X-,Y- and Z-Direction ($U_{wall}$ in X-Direction)', fontsize=10)
-
-        ax1.set_title('Y-Direction', fontsize=8)
-        ax1.set_ylabel('Height $y$')
-
-        ax1.plot(input_array[0, int(h/2), :,  int(h/2)],
-                 v_steps, label='t = 0')
-        ax1.plot(input_array[int(0.01*t), int(h/2), :, int(h/2)],
-                 v_steps, label=f't = {int(0.01*t)}')
-        ax1.plot(input_array[int(0.05*t), int(h/2), :, int(h/2)],
-                 v_steps, label=f't = {int(0.05*t)}')
-        ax1.plot(input_array[int(0.1*t), int(h/2), :, int(h/2)],
-                 v_steps, label=f't = {int(0.1*t)}')
-        ax1.plot(input_array[int(0.25*t), int(h/2), :, int(h/2)],
-                 v_steps, label=f't = {int(0.25*t)}')
-        ax1.plot(input_array[int(0.5*t), int(h/2), :, int(h/2)],
-                 v_steps, label=f't = {int(0.5*t)}')
-        ax1.plot(input_array[int(0.75*t), int(h/2), :, int(h/2)],
-                 v_steps, label=f't = {int(0.75*t)}')
-        ax1.plot(input_array[-1,  int(h/2), :,  int(h/2)],
-                 v_steps, label=f't = {t}')
-
-        ax2.set_title('X-Direction', fontsize=8)
-        ax2.set_ylabel('Length $x$')
-
-        ax2.plot(input_array[0, int(h/2), int(h/2), :], v_steps, label='t = 0')
-        print('test length values')
-        print(input_array[10, int(h/2), int(h/2), :])
-        ax2.plot(input_array[int(0.01*t), int(h/2), int(h/2), :],
-                 v_steps, label=f't = {int(0.01*t)}')
-        ax2.plot(input_array[int(0.05*t), int(h/2), int(h/2), :],
-                 v_steps, label=f't = {int(0.05*t)}')
-        ax2.plot(input_array[int(0.1*t), int(h/2), int(h/2), :],
-                 v_steps, label=f't = {int(0.1*t)}')
-        ax2.plot(input_array[int(0.25*t), int(h/2), int(h/2), :],
-                 v_steps, label=f't = {int(0.25*t)}')
-        ax2.plot(input_array[int(0.5*t), int(h/2), int(h/2), :],
-                 v_steps, label=f't = {int(0.5*t)}')
-        ax2.plot(input_array[int(0.75*t), int(h/2), int(h/2), :],
-                 v_steps, label=f't = {int(0.75*t)}')
-        ax2.plot(input_array[-1, int(h/2), int(h/2), :],
-                 v_steps, label=f't = {t}')
-
-        ax3.set_title('Z-Direction', fontsize=8)
-        ax3.set_ylabel('Depth $z$')
-
-        ax3.plot(input_array[0, :, int(h/2), int(h/2)], v_steps, label='t = 0')
-        print('test depth values')
-        print(input_array[10, :, int(h/2), int(h/2)])
-        ax3.plot(input_array[int(0.01*t), :, int(h/2), int(h/2)],
-                 v_steps, label=f't = {int(0.01*t)}')
-        ax3.plot(input_array[int(0.05*t), :, int(h/2), int(h/2)],
-                 v_steps, label=f't = {int(0.05*t)}')
-        ax3.plot(input_array[int(0.1*t), :, int(h/2), int(h/2)],
-                 v_steps, label=f't = {int(0.1*t)}')
-        ax3.plot(input_array[int(0.25*t), :, int(h/2), int(h/2)],
-                 v_steps, label=f't = {int(0.25*t)}')
-        ax3.plot(input_array[int(0.5*t), :, int(h/2), int(h/2)],
-                 v_steps, label=f't = {int(0.5*t)}')
-        ax3.plot(input_array[int(0.75*t), :, int(h/2), int(h/2)],
-                 v_steps, label=f't = {int(0.75*t)}')
-        ax3.plot(input_array[-1, :, int(h/2), int(h/2)],
-                 v_steps, label=f't = {t}')
-
-        plt.xlabel('Velocity $u$')
-        plt.legend()
-        plt.show()
-
-
-def compareFlowProfile(prediction_array, target_array, wall_height=20, u_wall=10, sigma=0.3):
-    t, h, w = prediction_array.shape
-    v_step = wall_height / (w-1)
-    v_steps = np.arange(0, wall_height + v_step, v_step).tolist()
-
-    for i in range(t):
-        fig, ax = plt.subplots()
-        ax.plot(v_steps, prediction_array[i][:, int(
-            w/2)], label='predicted profile')
-        ax.plot(v_steps, target_array[i][:, int(
-            w/2)], label='noisy analytical profile')
-        plt.yticks(range(int(-u_wall), int(u_wall*(2)+1), 5))
-        plt.xlabel('Height $z$')
-        plt.ylabel('Velocity $u$')
-        plt.title('The Startup Couette Problem')
-        plt.legend()
-        # fig.set_size_inches(5, 5)
-        plt.show()
-        fig.savefig(f'pred_vs_noisy_target_3e-1_{i}.svg')
-
-
-def plotVelocityField(input_array, wall_height=20):
-    if input_array.ndim == 2:
-        h, w = input_array.shape
-        v_step = wall_height / (h-1)
-        v_steps = np.arange(0, wall_height + v_step, v_step).tolist()
-
-        X1, Y1 = np.meshgrid(v_steps, v_steps)
-        u = input_array
-        v = np.zeros(shape=u.shape)
-
-        # set color field for better visualisation
-        n = -2
-        color = np.sqrt(((v-n)/2)*2 + ((u-n)/2)*2)
-
-        # set plot parameters
-        # u - velocity component in x-direction
-        # v - velocity component in y-direction
-        fig, ax = plt.subplots()
-        ax.quiver(X1, Y1, u, v, color, alpha=0.75)
-
-        plt.ylabel('Height $z$')
-        plt.xlabel('Depth $x$')
-        plt.title('The Startup Couette Velocity Field (Cross-Section)')
-        plt.show()
-
-    if input_array.ndim == 3:
-        d, h, w = input_array.shape
-        v_step = wall_height / (h-1)
-        v_steps = np.arange(0, wall_height + v_step, v_step).tolist()
-
-        X1, Y1 = np.meshgrid(v_steps, v_steps)
-        u = input_array[int(0.5*d)]
-        v = np.zeros(shape=u.shape)
-
-        # set color field for better visualisation
-        n = -2
-        color = np.sqrt(((v-n)/2)*2 + ((u-n)/2)*2)
-
-        # set plot parameters
-        # u - velocity component in x-direction
-        # v - velocity component in y-direction
-        fig, ax = plt.subplots()
-        ax.quiver(X1, Y1, u, v, color, alpha=0.75)
-
-        plt.ylabel('Height $z$')
-        plt.xlabel('Depth $x$')
-        plt.title('The Startup Couette Velocity Field (Cross-Section)')
-        plt.show()
-
-
 def compareVelocityField(prediction_array, target_array, wall_height=20):
     t, h, w = prediction_array.shape
     v_step = wall_height / (h-1)
@@ -283,7 +52,7 @@ def compareVelocityField(prediction_array, target_array, wall_height=20):
 def expandVector2Matrix(input_list):
     output_list = []
     N = input_list[0].shape[0]
-    print('dimensions of array in list: ', N)
+    # print('dimensions of array in list: ', N)
 
     for element in input_list:
         dummy = (np.vstack([element]*N)).T
@@ -373,8 +142,8 @@ def my1DCouetteSolver(desired_timesteps, u_wall=10, wall_height=20, nu=2, vertic
                             wall_height, nu, vertical_resolution, sigma)
     my_1D_array = list2array(my_data)
 
-    if sigma != 0:
-        my_1D_array = applyNoise(my_1D_array, (sigma*u_wall))
+    if sigma != 0.0:
+        my_1D_array = applyNoise(my_1D_array, sigma)
 
     return my_1D_array
 
@@ -389,7 +158,7 @@ def my2DCouetteSolver(desired_timesteps, u_wall=10, wall_height=20, nu=2, vertic
 
     my_2d_array = list2array(my_2d_list)
 
-    if sigma != 0:
+    if sigma != 0.0:
         my_2d_array = applyNoise(my_2d_array, sigma)
 
     # Sanity check: Plot the flow data
@@ -409,7 +178,7 @@ def my3DCouetteSolver(desired_timesteps, u_wall=10, wall_height=20, nu=2, vertic
 
     my_3d_array = list2array(my_3d_list)
 
-    if sigma != 0:
+    if sigma != 0.0:
         my_3d_array = applyNoise(my_3d_array, sigma)
 
     # Sanity check: Plot the flow data
